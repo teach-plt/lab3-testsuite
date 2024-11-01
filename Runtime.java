8aa8c02810ee5b086ddbe0660aae3c19df0cb01a
Andreas Abel <andreas.abel@ifi.lmu.de>
2024-11-01 11:06:29 +0100

    Runtime.java: force decimal dot setting locale to Locale.ROOT
    Setting the locale at each invokation of printDouble is certainly ugly
    and inefficient, but I did not find a way to reproduce exactly the
    behavior of print(double) just without a locale-specific decimal separator.

--- Runtime.java
+++ Runtime.java
@@ -12,7 +12,20 @@ public class Runtime {
 
     // Use the ROOT locale to force a decimal dot rather than the country-specific decimal separator.
     public static void printDouble (double x) {
-        System.out.println(String.format(Locale.ROOT, "%f", x));
+        // The following attempt fixes the precision to 5 digits
+        // and thus differs from a vanilla println(x).
+        //
+        // System.out.println(String.format(Locale.ROOT, "%f", x));
+        //
+        // I did not find an way to reproduce exactly the behavior of println(x)
+        // using String.format.  Someone may find one, but I ran out of steam.
+        // (E.g. I was suprised to learn that the code of Double.toString(),
+        // invoked by println(double), does not invoke a formatter like String.format
+        // but is instead a very ugly custom code: getBinaryToASCIIConverter(double).)
+        //
+        // Thus, for now I resort to the hacky solution to just globally set the locale.
+        Locale.setDefault(Locale.ROOT);
+        System.out.println(x);
     }
 
     public static int readInt () {
