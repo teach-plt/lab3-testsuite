df186544cd8bf887783f914196dc306028cb4289
Andreas Abel <andreas.abel@ifi.lmu.de>
2024-11-18 15:58:44 +0100

    run-test-FOO.sh: forward all arguments to plt-test-lab3

--- run-test-cabal.sh
+++ run-test-cabal.sh
@@ -2,10 +2,12 @@
 
 if [ "$1" == "" -o  "$1" == "-h" -o "$1" == "--help" ]; then
   echo "PLT lab 3 testsuite runner"
-  echo "usage: $0 DIRECTORY"
+  echo "usage: $0 [OPTIONS] DIRECTORY"
+  echo "Takes the same options as plt-test-lab3:"
+  cabal run plt-test-lab3
   exit 1
 fi
 
-cabal run plt-test-lab3 -- "$1"
+cabal run plt-test-lab3 -- "$@"
 
 # EOF
--- run-test-ghc.sh
+++ run-test-ghc.sh
@@ -2,10 +2,12 @@
 
 if [ "$1" == "" -o  "$1" == "-h" -o "$1" == "--help" ]; then
   echo "PLT lab 3 testsuite runner"
-  echo "usage: $0 DIRECTORY"
+  echo "usage: $0 [OPTIONS] DIRECTORY"
+  echo "Takes the same options as plt-test-lab3:"
+  runghc plt-test-lab3
   exit 1
 fi
 
-runghc plt-test-lab3 -- "$1"
+runghc plt-test-lab3 -- "$@"
 
 # EOF
